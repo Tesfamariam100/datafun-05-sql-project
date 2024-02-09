cdc7ba9711e426814e156e31e2ee9b12e816a77d
Tesfamariam <s568695@nwmissouri.edu>
2024-02-08 23:47:42 -0500

    modified:   "db_initialize_tesfamariam.py"

---
+++ db_initialize_tesfamariam.py
@@ -0,0 +1,12 @@
+""" 
+Python and SQL project: db_initialize_tesfamariam.py
+
+This script contains the logic for initializing the database schema and populating it with initial data.
+
+Author: Tesfamariam
+Date: 2024-02-08
+
+Usage:
+- Run this script to create the database schema and populate it with initial data.
+- Make sure to review and adjust the SQL statements in the script according to your project requirements.
+"""
